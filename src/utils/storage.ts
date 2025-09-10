1f6251195eb162aec4c207199381e48500db748c
Christion <bubba7xallahan@gmail.com>
2025-09-10 16:29:40 -0500

    addRecipe

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -11,3 +11,14 @@ export function saveRecipes(list: Recipe[]) : void {
     localStorage.setItem(RECIPES_KEY, JSON.stringify(list));
 }
 
+export function addRecipe(recipe: Omit<Recipe, "id" | "createdAt">): Recipe {
+    const list = getRecipes();
+    const toSave: Recipe = {
+        id: crypto.randomUUID(),
+        createdAt: Date.now(),
+        ...recipe,
+    };
+    list.push(toSave);
+    saveRecipes(list);
+    return toSave;
+}
